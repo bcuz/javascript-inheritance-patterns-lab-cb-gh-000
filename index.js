afc7d72c79b6f50aed843a3b26f16e697f50b3d2
Adam N <adam.nawroc@gmail.com>
2019-01-01 12:29:14 +0000

    progress

--- index.js
+++ index.js
@@ -6,3 +6,85 @@ function Point(x,y) {
 Point.prototype.toString = function() {
   return `(${this.x}, ${this.y})`
 }
+
+function Side(length) {
+  this.length = length
+}
+
+function Shape() {
+}
+
+Shape.prototype.addToPlane = function(x,y) {
+  this.position = new Point(x,y)
+}
+
+Shape.prototype.move = function(x,y) {
+  this.position = new Point(x,y)
+}
+
+function Circle(radius) {
+  Shape.call(this)
+  this.radius = radius
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle
+
+Circle.prototype.area = function() {
+  return Math.PI * this.radius ** 2
+}
+
+Circle.prototype.circumference = function() {
+  return 2 * Math.PI * this.radius
+}
+
+Circle.prototype.diameter = function() {
+  return this.radius * 2
+}
+
+function Polygon(sides) {
+  Shape.call(this)
+  this.sides = sides
+}
+
+Polygon.prototype = Object.create(Shape.prototype);
+Polygon.prototype.constructor = Polygon
+
+Polygon.prototype.perimeter = function() {
+
+  let total = 0
+  for (let side of this.sides) {
+    total += side.length
+  }
+
+  return total
+}
+
+Polygon.prototype.numberOfSides = function() {
+  return this.sides.length
+}
+
+function Quadrilateral(side1, side2, side3, side4) {
+  Polygon.call(this, [new Side(side1), new Side(side2), new Side(side3), new Side(side4)])
+}
+
+Quadrilateral.prototype = Object.create(Polygon.prototype);
+Quadrilateral.prototype.constructor = Quadrilateral
+
+function Rectangle(width, height) {
+  Quadrilateral.call(this, width, width, height, height)
+  this.width = width
+  this.height = height
+}
+
+Rectangle.prototype = Object.create(Quadrilateral.prototype);
+Rectangle.prototype.constructor = Rectangle
+
+Rectangle.prototype.area = function() {
+  return this.width * this.height
+}
+
+function Square(length) {
+  Rectangle.call(this, length, length)
+  this.length = length
+}
